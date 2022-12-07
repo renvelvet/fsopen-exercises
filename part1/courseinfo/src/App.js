342c419156f01e7129e7fd202509b5d89696fadd
renvelvet <reshapuspita734@gmail.com>
2022-12-07 17:52:11 +0700

    change props to "parts"

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -46,14 +46,8 @@ const App = () => {
   return (
     <div>
       <Header course={course} />
-      <Content 
-        part1={part1} 
-        part2={part2} 
-        part3={part3}   
-        exercises1={exercises1} 
-        exercises2={exercises2} 
-        exercises3={exercises3} />
-      <Total total={exercises1+exercises2+exercises3} />
+      <Content parts={parts} />
+      <Total parts={parts} />
     </div>
   )
 }
