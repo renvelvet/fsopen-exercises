687234a429a41132cf1cd4621a63ac8b85baacd7
renvelvet <reshapuspita734@gmail.com>
2022-12-07 17:50:17 +0700

    change 3 objects into am array

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -30,18 +30,18 @@ const Total = (props) => {
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
+  const parts = [{
     name: 'Fundamentals of React',
     exercises: 10
-  } 
-  const part2 = {
+  }, 
+  {
     name: 'Using props to pass data',
     exercises: 7
-  } 
-  const part3 = {
+  }, 
+  {
     name: 'State of a component',
     exercises: 14
-  } 
+  }]
 
   return (
     <div>
