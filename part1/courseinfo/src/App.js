86834b70127bfbdb743f4b1e4f52a1e669beb98c
renvelvet <reshapuspita734@gmail.com>
2022-12-20 11:38:19 +0700

    refactor courseinfo part 1

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,60 +1,43 @@
-const Header = ({ course }) => {
-  return <h1>{course.name}</h1>;
-};
+const Header = ({ course }) => <h1>{course}</h1>;
 
-const Part = ({ part }) => {
-  return (
-    <p>
-      {part.name} {part.exercises}
-    </p>
-  );
-};
+const Part = ({ part }) => (
+  <p>
+    {part.name} {part.exercises}
+  </p>
+);
 
-const Content = ({ parts }) => {
-  return (
-    <div>
-      <Part part={parts[0]} />
-      <Part part={parts[1]} />
-      <Part part={parts[2]} />
-    </div>
-  );
-};
+const Content = ({ parts }) => (
+  <>
+    <Part part={parts[0]} />
+    <Part part={parts[1]} />
+    <Part part={parts[2]} />
+  </>
+);
 
-const Total = ({ parts }) => {
-  const calculateParts = () => {
-    let sum = 0;
-    parts.forEach((part) => (sum += part.exercises));
-
-    return sum;
-  };
-
-  return <p>Number of exercises {calculateParts()}</p>;
-};
+const Total = ({ sum }) => <p>Number of exercises {sum}</p>;
 
 const App = () => {
-  const course = {
-    name: "Half Stack application development",
-    parts: [
-      {
-        name: "Fundamentals of React",
-        exercises: 10,
-      },
-      {
-        name: "Using props to pass data",
-        exercises: 7,
-      },
-      {
-        name: "State of a component",
-        exercises: 14,
-      },
-    ],
-  };
+  const course = "Half Stack application development";
+  const parts = [
+    {
+      name: "Fundamentals of React",
+      exercises: 10,
+    },
+    {
+      name: "Using props to pass data",
+      exercises: 7,
+    },
+    {
+      name: "State of a component",
+      exercises: 14,
+    },
+  ];
 
   return (
     <div>
       <Header course={course} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
+      <Content parts={parts} />
+      <Total sum={parts.reduce((sum, part) => sum + part.exercises, 0)} />
     </div>
   );
 };
