770fe1a40cf0f950154c910c1100b3bb86881407
renvelvet <reshapuspita734@gmail.com>
2023-01-22 15:41:01 +0700

    exercises 3.13, 3.14

--- part3/be-phonebook/index.js
+++ part3/be-phonebook/index.js
@@ -1,3 +1,6 @@
+require('dotenv').config();
+const Person = require('./models/person');
+
 const express = require('express');
 const cors = require('cors');
 const app = express();
@@ -6,74 +9,40 @@ app.use(express.json());
 app.use(cors());
 app.use(express.static('build'));
 
-let persons = [
-  {
-    id: 1,
-    name: 'Arto Hellas',
-    number: '040-123456',
-  },
-  {
-    id: 2,
-    name: 'Ada Lovelace',
-    number: '39-44-5323523',
-  },
-  {
-    id: 3,
-    name: 'Dan Abramov',
-    number: '12-43-234345',
-  },
-  {
-    id: 4,
-    name: 'Mary Poppendieck',
-    number: '39-23-6423122',
-  },
-];
-
 function generateID() {
   const id = Math.random() * 1000000000;
   return id;
 }
 
 app.get('/api/persons', (request, response) => {
-  response.json(persons);
+  Person.find({}).then((person) => {
+    response.json(person);
+  });
+});
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body;
+
+  if (body.name === undefined) {
+    return response.status(400).json({ error: 'content missing' });
+  }
+
+  const newPerson = new Person({
+    name: body.name,
+    number: body.number,
+  });
+
+  newPerson.save().then((addedPerson) => response.json(addedPerson));
 });
 
 app.get('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id);
-  const person = persons.find((person) => person.id === id);
-
-  if (!person) {
-    return response.status(404).end();
-  }
-
-  response.json(person);
+  Person.findById(request.params.id).then((person) => response.json(person));
 });
 
 app.get('/info', (request, response) => {
   const date = new Date().toString();
   response.send(`<p>Phonebook has info for ${persons.length} people</p>
   <p>${date}</p>`);
-});
-app.post('/api/persons', (request, response) => {
-  const body = request.body;
-  const isFound = persons.find(
-    (person) => person.name.toLowerCase() === body.name.toLowerCase()
-  );
-
-  if (!body.name || !body.number || isFound) {
-    return response.status(400).json({
-      error: 'name must be unique',
-    });
-  }
-
-  const person = {
-    id: generateID(),
-    name: body.name,
-    number: body.number,
-  };
-
-  persons = persons.concat(person);
-  response.send(body);
 });
 
 app.delete('/api/persons/:id', (request, response) => {
