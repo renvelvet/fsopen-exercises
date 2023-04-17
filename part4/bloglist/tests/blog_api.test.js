975cb950fd2d85c981ff0d713cc799cd60c66565
renvelvet <reshapuspita734@gmail.com>
2023-04-17 11:36:57 +0700

    4.11*: Blog list tests, step4

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -17,6 +17,27 @@ test('verifies that the unique identifier property of the blog posts is named id
   res.body.forEach((element) => expect(element.id).toBeDefined());
 });
 
+test('verifies that if the likes property is missing from the request, it will default to the value 0', async () => {
+  const newBlog = {
+    title: 'Discover Myths to Improve Your English Communication',
+    author: 'Nur Fauziyyaaa',
+    url: 'https://medium.com/generation-girl/discover-myths-to-improve-your-english-communication-91a72a07f9b6',
+  };
+
+  const res = await api
+    .post('/api/blogs')
+    .send({
+      title: newBlog.title,
+      author: newBlog.author,
+      url: newBlog.url,
+      likes: newBlog.likes || 0,
+    })
+    .set('Accept', 'application/json')
+    .expect('Content-Type', /json/)
+    .expect(201);
+  console.log('res', res.body);
+});
+
 afterAll(async () => {
   await mongoose.connection.close();
 });
