c4ba7e19554655398d516eac36a8b380f3a84775
renvelvet <reshapuspita734@gmail.com>
2023-04-17 11:19:00 +0700

    4.9: Blog list tests, step2

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -11,6 +11,12 @@ test('list are returned as json', async () => {
     .expect('Content-Type', /application\/json/);
 }, 100000);
 
+test('verifies that the unique identifier property of the blog posts is named id', async () => {
+  const res = await api.get('/api/blogs');
+
+  res.body.forEach((element) => expect(element.id).toBeDefined());
+});
+
 afterAll(async () => {
   await mongoose.connection.close();
 });
