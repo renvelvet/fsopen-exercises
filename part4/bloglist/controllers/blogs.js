e500e1ce55316f3d0ed1f78405c575abf44c9d65
renvelvet <reshapuspita734@gmail.com>
2023-04-27 16:24:16 +0700

    4.17: bloglist expansion, step5

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -1,20 +1,44 @@
 const blogsRouter = require('express').Router();
 const Blog = require('../models/blog');
+const User = require('../models/user');
+const jwt = require('jsonwebtoken');
 
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({});
+  const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 });
   response.json(blogs);
 });
 
-blogsRouter.post('/', (request, response, next) => {
-  const blog = new Blog(request.body);
+const getTokenFrom = (request) => {
+  const authorization = request.get('authorization');
 
-  blog
-    .save()
-    .then((result) => {
-      response.status(201).json(result);
-    })
-    .catch((exception) => next(exception));
+  if (authorization && authorization.startsWith('Bearer ')) {
+    return authorization.replace('Bearer ', '');
+  }
+
+  return null;
+};
+
+blogsRouter.post('/', async (request, response, next) => {
+  const user = await User.find({});
+  const { title, author, url, likes } = request.body;
+  const blog = new Blog({ title, url, author, likes, user: user[0].id });
+
+  /* 
+  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET);
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token invalid' });
+  }
+  const user = await User.findById(decodedToken.id);
+ */
+  try {
+    const savedBlog = await blog.save();
+    user[0].blogs = user[0].blogs.concat(savedBlog._id);
+    await user[0].save();
+
+    response.status(201).json(savedBlog);
+  } catch (error) {
+    next(error);
+  }
 });
 
 blogsRouter.delete('/:id', async (request, response) => {
