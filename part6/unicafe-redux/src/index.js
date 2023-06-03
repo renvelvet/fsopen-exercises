79c88c93e3a34f184ea0b46541077bede582327c
renvelvet <reshapuspita734@gmail.com>
2023-06-03 17:51:10 +0700

    6.2: unicafe revisited, step2

--- part6/unicafe-redux/src/index.js
+++ part6/unicafe-redux/src/index.js
@@ -1,75 +1,79 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { counterReducer } from './reducer';
-import { configureStore } from '@reduxjs/toolkit';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { counterReducer } from './reducer'
+import { configureStore } from '@reduxjs/toolkit'
 
-const store = configureStore({ reducer: counterReducer });
+const store = configureStore({ reducer: counterReducer })
 
-const Display = ({ text }) => <h1>{text}</h1>;
+const Display = ({ text }) => <h1>{text}</h1>
 
-const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>;
+const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
 
 const StatisticLine = ({ text, value }) => (
   <tr>
     <td>{text}</td>
     <td>{value}</td>
   </tr>
-);
+)
 
 const Statistics = ({ feedback }) => {
   const all = () => {
-    const sumAll = Object.values(feedback).reduce((a, b) => a + b, 0);
-    return sumAll;
-  };
+    const sumAll = Object.values(feedback).reduce((a, b) => a + b, 0)
+    return sumAll
+  }
 
   const average = (total) => {
     const average =
-      total === 0 ? 0 : (feedback.good * 1 - feedback.bad * 1) / total;
-    return average;
-  };
+      total === 0 ? 0 : (feedback.good * 1 - feedback.bad * 1) / total
+    return average
+  }
 
   const positive = (total) => {
-    const positive = feedback.good === 0 ? 0 : (feedback.good / total) * 100;
+    const positive = feedback.good === 0 ? 0 : (feedback.good / total) * 100
     return Intl.NumberFormat('default', {
       style: 'percent',
       minimumFractionDigits: 2,
       maximumFractionDigits: 2,
-    }).format(positive / 100);
-  };
+    }).format(positive / 100)
+  }
 
   if (all() === 0) {
-    return <p>No feedback given</p>;
+    return <p>No feedback given</p>
   } else {
     return (
       <table>
         <tbody>
           <StatisticLine text="good" value={feedback.good} />
-          <StatisticLine text="neutral" value={feedback.neutral} />
+          <StatisticLine text="ok" value={feedback.ok} />
           <StatisticLine text="bad" value={feedback.bad} />
           <StatisticLine text="all" value={all()} />
           <StatisticLine text="average" value={average(all())} />
           <StatisticLine text="positive" value={positive(all())} />
         </tbody>
       </table>
-    );
+    )
   }
-};
+}
 
 const App = () => {
   return (
     <div>
       <Display text="give feedback" />
       <Button onClick={() => store.dispatch({ type: 'GOOD' })} text="good" />
-      <Button onClick={() => store.dispatch({ type: 'OK' })} text="neutral" />
+      <Button onClick={() => store.dispatch({ type: 'OK' })} text="ok" />
       <Button onClick={() => store.dispatch({ type: 'BAD' })} text="bad" />
+      <Button
+        onClick={() => store.dispatch({ type: 'ZERO' })}
+        text="reset stats"
+      />
       <Display text="statistics" />
       <Statistics feedback={store.getState()} />
     </div>
-  );
-};
+  )
+}
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-const renderApp = () => root.render(<App />);
+const root = ReactDOM.createRoot(document.getElementById('root'))
+const renderApp = () => root.render(<App />)
 
-renderApp();
-store.subscribe(renderApp);
+renderApp()
+store.subscribe(renderApp)
--- part6/unicafe-redux/src/reducer.js
+++ part6/unicafe-redux/src/reducer.js
@@ -2,21 +2,21 @@ const initialState = {
   good: 0,
   bad: 0,
   ok: 0,
-};
+}
 
 export const counterReducer = (state = initialState, action) => {
   switch (action.type) {
     case 'GOOD':
-      return { ...state, good: state.good + 1 };
+      return { ...state, good: state.good + 1 }
     case 'OK':
-      return state;
+      return { ...state, ok: state.ok + 1 }
     case 'BAD':
-      return { ...state, bad: state.bad - 1 };
+      return { ...state, bad: state.bad + 1 }
     case 'ZERO':
-      return state;
+      return { ...state, bad: 0, ok: 0, good: 0 }
     case 'DO NOTHING':
-      return state;
+      return state
     default:
-      return state;
+      return state
   }
-};
+}
