b38ec4556752fa5877563c8a692a835d436bf4b7
renvelvet <reshapuspita734@gmail.com>
2023-10-16 22:27:40 +0700

    8.4: Books of an author

--- part8/graphql_practice/index.js
+++ part8/graphql_practice/index.js
@@ -108,7 +108,7 @@ const typeDefs = `
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks: [Book!]!
+    allBooks(author:String): [Book!]!
     allAuthors: [Author!]
   }
 `
@@ -117,7 +117,9 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: () => books,
+    allBooks: (root, args) => {
+      return books.filter((book) => book.author === args.author)
+    },
     allAuthors: (root, args) => {
       return authors.map((author) => {
         let total = 0
